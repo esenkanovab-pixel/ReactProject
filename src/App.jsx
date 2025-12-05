cbaa7851ee83d3070628bc7ce1ce7c99dd272d4d
esenkanovab-pixel <esenkanov_ab@iuca.kg>
2025-12-05 17:01:37 +0600

    Подключение шапки и подвала в макет приложения

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import List from "./pages/List";
 import Detail from "./pages/Detail";
+import Header from "./components/Header"; // 👈 Импортируем
+import Footer from "./components/Footer"; // 👈 Импортируем
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<List />} />
-      <Route path="/faction/:id" element={<Detail />} />
-    </Routes>
+    <div className="app-wrapper">
+      <Header />
+      
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/faction/:id" element={<Detail />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
